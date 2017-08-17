ddf8a3ee63883ee90cb5262fec4f0433360135db
Joffily Ferreira <joffily@outlook.com>
2017-08-17 20:17:29 -0300

    Adicionado método para buscar livros pelo ISBN

--- src/main/java/br/edu/ifpb/dao/LivroDAO.java
+++ src/main/java/br/edu/ifpb/dao/LivroDAO.java
@@ -1,9 +1,27 @@
 package br.edu.ifpb.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import br.edu.ifpb.entity.Livro;
 
 public class LivroDAO extends DAO<Livro> {
 	public LivroDAO() {
 		super();
+	}
+
+	public Livro findByISBN(String isbn) {
+		Query q = manager.createQuery("select l from Livro l where l.isbn = :isbn");
+		q.setParameter("isbn", isbn);
+
+		@SuppressWarnings("unchecked")
+		List<Livro> livros = q.getResultList();
+
+		if (livros.isEmpty()) {
+			return null;
+		}
+
+		return livros.get(0);
 	}
 }
